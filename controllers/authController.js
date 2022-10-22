3adff892797d9dfe1237b7eeaf40f231d4734be3
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-10-22 08:42:57 +0545

    Added signup feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,23 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const handleError = (err) => {
+    const errors = {
+        email: '',
+        password: ''
+    }
+    if (err.code === 11000) {
+        errors.email = "Email already exists"
+    }
+
+    if (err._message === "User validation failed") {
+        Object.values(err.errors).map((error) => {
+            const { properties } = error;
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
 
 const returnSignupPage = (req, res) => {
     res.render('signup')
@@ -12,11 +31,16 @@ const createUser = async (req, res) => {
     try {
         //Creates a user in database and returns the instance
         const user = await User.create(req.body)
+
+        const token = jwt.sign({ user: user._id }, process.env.TOKEN_SECRET, {
+            expiresIn: '1d'
+        })
+        res.cookie('jwt', token);
         //Sending the id of new user to the frontend
         res.json({user : user._id})
     } catch (err) {
-        console.log(err)
-        res.json({errors : "Error occured"})
+        const errors = handleError(err);
+        res.json({errors})
     }
 }
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
   //Middlewares
+  const { jwt: token } = req.cookies;
+  if (token) {
+    const isValid = jwt.verify(token, process.env.TOKEN_SECRET);
+    if (isValid) {
+      next();
+    }
+  }
+  res.redirect("/login");
 };
 
 module.exports = { requireAuth };
